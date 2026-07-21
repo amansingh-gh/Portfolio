fee846b128b5f92ade640a94207fd2a3035eb299
AMN-TUF <yo.akaman@gmail.com>
2026-07-22 00:48:44 +0530

    Adding Splash Cursor

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import SplashCursor from './app/components/SplashCursor.jsx';
+createRoot(document.getElementById("root")!).render(
+<>
+<SplashCursor
+  DENSITY_DISSIPATION={3.5}
+  VELOCITY_DISSIPATION={2}
+  PRESSURE={0.1}
+  CURL={3}
+  SPLAT_RADIUS={0.2}
+  SPLAT_FORCE={2000}
+  COLOR_UPDATE_SPEED={7}
+  SHADING
+  RAINBOW_MODE={true}
+  COLOR="#A855F7"
+/>
+<App />
+</>
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+);
